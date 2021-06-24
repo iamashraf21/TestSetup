14e7de175a699a3aa3d4f1f7d91dbebd344ac944
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-24 19:52:20 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -9,11 +9,8 @@ cd $GITHUB_WORKSPACE
 
 # Link Arduino library
 #ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
+ls -l /dev/serial/
 
-echo "Printing pwd"
-pwd
-echo "listing files---"
-ls -Rlh
 cd $HOME/Documents/myagent/_work/1/s/test/
 arduino-cli compile -b Seeeduino:samd:zero -e
 arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
