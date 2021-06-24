47af6dd5cf279954b256d073c268d548d3b79ec5
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-24 20:39:16 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -15,6 +15,7 @@ whoami
 cd $HOME/Documents/myagent/_work/1/s/test/
 arduino-cli compile -b Seeeduino:samd:zero -e
 arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
+rm -r test-results.xml
 sleep 5
 python SerialRead.py
 
