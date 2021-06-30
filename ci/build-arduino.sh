dc2761831abc358bcaa0db7b400f1f3c510e304d
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-30 17:56:53 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -7,11 +7,6 @@ shopt -s globstar
 # Make sure we are inside the github workspace
 cd $GITHUB_WORKSPACE
 
-# Link Arduino library
-#ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
-ls -l /dev/serial/by-path
-whoami
-
 cd $HOME/Documents/myagent/_work/1/s/test/
 arduino-cli compile -b Seeeduino:samd:zero -e
 arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
